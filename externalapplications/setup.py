aec6db76b95b3f0961ddba8b23fdcae125cc2ffa
Henning Bredel <h.bredel@52north.org>
2022-11-25 16:01:22 +0100

    Add setup.py§

---
+++ externalapplications/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='externalapplications',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GPL',
+    description='GeoNode External Applications',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='http://52north.org',
+    author='52North Developers',
+    author_email='info@52north.org',
+    install_requires=[
+    ],
+)
