1b8ce1977027ec1e3192b08c88103520d0f65e60
Henning Bredel <h.bredel@52north.org>
2022-11-30 15:31:28 +0000

    Display the correct metadata page URL

--- externalapplications/externalapplications/models.py
+++ externalapplications/externalapplications/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-from django.urls import reverse
+from django.conf import settings
 from geonode.geoapps.models import GeoApp
 
 # Create your models here.
 class ExternalApplication(GeoApp):
 
     url = models.URLField(max_length=2000, null=False, blank=False, help_text="Link to the external application")
+
+    def get_detail_url(self):
+        site_url = settings.SITEURL.rstrip('/') if settings.SITEURL.startswith('http') else settings.SITEURL
+        return f"{site_url}/external-applications/{self.pk}"
 
     def get_absolute_url(self):
         return self.url
