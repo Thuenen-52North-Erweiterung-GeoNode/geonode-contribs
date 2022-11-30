f06e6a84f1d3290d06d7f006daf896caf23a8e30
Henning Bredel <h.bredel@52north.org>
2022-11-30 16:32:41 +0000

    No iframe embed for external application

--- externalapplications/externalapplications/models.py
+++ externalapplications/externalapplications/models.py
@@ -7,6 +7,10 @@ class ExternalApplication(GeoApp):
 
     url = models.URLField(max_length=2000, null=False, blank=False, help_text="Link to the external application")
 
+    @property
+    def embed_url(self):
+        return ""
+
     def get_detail_url(self):
         site_url = settings.SITEURL.rstrip('/') if settings.SITEURL.startswith('http') else settings.SITEURL
         return f"{site_url}/external-applications/{self.pk}"
