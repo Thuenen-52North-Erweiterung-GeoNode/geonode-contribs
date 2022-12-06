c0646e6b1609d58ee482b384005b3d66d5b38761
Henning Bredel <h.bredel@52north.org>
2022-12-06 09:47:41 +0100

    Clean up a bit

--- externalapplications/externalapplications/static/mapstore/extensions/README.md
+++
@@ -1,4 +0,0 @@
-# MapStore Client Extension
-
-Contains the mapstore-client built extension for external applications.
-The extension has to be built in the `mapstore-extension` folder.
--- externalapplications/externalapplications/urls.py
+++ externalapplications/externalapplications/urls.py
@@ -1,13 +1,8 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^create/?$', views.create_external_application, name='external_application_create'),
-    url(r'^(?P<appid>\d+)/metadata$',
-        views.external_application_metadata, name='external_application_metadata'),
-    # url(r'^(?P<appid>[^/]+)/metadata_detail$', views.external_application_metadata_detail,
-    #     name='external_application_metadata_detail'),
-    # url(r'^(?P<appid>\d+)/metadata_advanced$', views.external_application_metadata_advanced,
-    #     name='external_application_metadata_advanced'),
-    #url(r'^', include('externalapplications.api.urls')),
+    url(r'^(?P<appid>[^/]+)/metadata_detail$', views.external_application_metadata_detail,
+        name='external_application_metadata_detail'),
 ]
